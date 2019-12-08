a156cb8c6b38d994bfa140ba408a4a401a07292b
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-12-08 14:29:57 +0100

    Removed some dead code
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- spwc/common/__init__.py
+++ spwc/common/__init__.py
@@ -1,18 +1,5 @@
-import diskcache as dc
-from appdirs import user_cache_dir
 from datetime import datetime, timezone
 from dateutil.parser import parse
-
-cache = dc.Cache(user_cache_dir("SciQLop", "LPP"))
-
-
-def get_cache_entries(server, product_name, start_date, stop_date):
-    pass
-
-
-def add_cache_entry(server, product_name, dataframe):
-    pass
-
 
 def listify(obj: list or object) -> list:
     if type(obj) is list:
