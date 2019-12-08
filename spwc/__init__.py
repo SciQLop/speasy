84d517c8c65e4f93281cfae1f887822731965f01
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-12-08 18:41:02 +0100

    Forgot to use bumpversion :(
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- spwc/__init__.py
+++ spwc/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Alexis Jeandet"""
 __email__ = 'alexis.jeandet@member.fsf.org'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 from .common.variable import SpwcVariable
 from .amda import AMDA
 from .cdaweb import cdaweb as cd
