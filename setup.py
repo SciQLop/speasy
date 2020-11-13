e3a1bcfdc44c2a70066924e354b2064f1eddae0c
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-11-13 18:48:02 +0100

    Update project URL to SciQLop project
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/jeandet/spwc',
+    url='https://github.com/SciQLop/spwc',
     version='0.7.0',
     zip_safe=False,
 )
