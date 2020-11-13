ade6c4a0f8c3e152daaabde4844ef87fc11a2faa
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-11-13 18:48:28 +0100

    Bump version: 0.7.0 → 0.7.1

--- spwc/__init__.py
+++ spwc/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Alexis Jeandet"""
 __email__ = 'alexis.jeandet@member.fsf.org'
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 from .common.variable import SpwcVariable
 from .amda import AMDA
