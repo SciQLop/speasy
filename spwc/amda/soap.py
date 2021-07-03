2a5de040dec4ab98c305198dfc0e00d9dd9b9bcf
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2021-07-03 17:14:10 +0200

    Add deprecation message for AMDA SOAP client
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- spwc/amda/soap.py
+++ spwc/amda/soap.py
@@ -1,5 +1,5 @@
 from zeep import Client
-
+from ..common import deprecation
 
 class AmdaSoap:
     def __init__(self, server_url="http://amda.irap.omp.eu", wsdl='AMDA/public/wsdl/Methods_AMDA.wsdl', strict=True):
@@ -7,6 +7,7 @@ class AmdaSoap:
         self.server_url = server_url
 
     def get_parameter(self, **kwargs):
+        deprecation("SOAP protocol will be removed soon")
         resp = self.soap_client.service.getParameter(**kwargs).__json__()
         if resp["success"]:
             return resp["dataFileURLs"][0]
@@ -14,6 +15,7 @@ class AmdaSoap:
             return None
 
     def get_obs_data_tree(self):
+        deprecation("SOAP protocol will be removed soon")
         resp = self.soap_client.service.getObsDataTree().__json__()
         if resp["success"]:
             return resp["WorkSpace"]["LocalDataBaseParameters"]
