9bd9655f741be7a943f1a70aee48bdb6dda75ff9
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2021-08-04 23:05:57 +0200

    Introduced amda.TimetableIndex to replace xmlid a string
    AMDA timetables xmlid are sharedtimeTable_[0:N] which is not much handy
    alone.
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy/amda/timetable.py
+++ speasy/amda/timetable.py
@@ -4,13 +4,24 @@ import numpy as np
 
 from speasy.common.variable import SpeasyVariable
 
+
+class TimetableIndex:
+    def __init__(self, uid, name):
+        self.uid = uid
+        self.name = name
+
+    def __repr__(self):
+        return f'<TimetableIndex: {self.name}, id: {self.uid}>'
+
+
 class TimeTable(SpeasyVariable):
     """AMDA TimeTable container. Timetables contain two time columns one for the begining of an
-    event and one for the end. 
+    event and one for the end.
     """
     pass
-    #def __init__(self, time=np.empty(0), data=np.empty((0, 1)), meta=None, columns=None):
+    # def __init__(self, time=np.empty(0), data=np.empty((0, 1)), meta=None, columns=None):
     #    super().__init__(time=time,data=data,meta=meta,columns=columns)
+
 
 class Catalog(TimeTable):
     """AMDA Catalog container. Catalogs are TimeTables with added columns.
