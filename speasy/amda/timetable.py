a7771627d9b9e1c69af67d0afaf29ee98d89b2cb
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2021-08-05 22:02:10 +0200

    AMDA: some cleanups plus introduced DatasetIndex (same idea than TimetableIndex)
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy/amda/timetable.py
+++ speasy/amda/timetable.py
@@ -1,17 +1,10 @@
 """AMDA Timetable definition. Serves a cosmetic need for now, functionality to be added in the future.
 """
-import numpy as np
 
 from speasy.common.variable import SpeasyVariable
 
 
-class TimetableIndex:
-    def __init__(self, uid, name):
-        self.uid = uid
-        self.name = name
 
-    def __repr__(self):
-        return f'<TimetableIndex: {self.name}, id: {self.uid}>'
 
 
 class TimeTable(SpeasyVariable):
