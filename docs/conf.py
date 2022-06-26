d6ac8aaf57394dabec9ec6b7320fbfc4f324a372
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-06-26 18:35:19 +0200

    [Doc] set language to avoid sphinx warning

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ os.environ['PYTHONPATH'] = src_path
 #
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
@@ -130,7 +129,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
