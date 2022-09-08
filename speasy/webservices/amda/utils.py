Fixes all tests and complete SpeasyVariable model migration, do not force anymore Astropy units on ssc coordinates, makes it an opt-in option feature
Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

speasy/webservices/amda/utils.py
@@ -5,7 +5,7 @@ conversion procedures for parsing CSV and VOTable data.
 import os
 import datetime
 from urllib.request import urlopen
-from speasy.products.variable import SpeasyVariable
+from speasy.products.variable import SpeasyVariable, VariableAxis, VariableTimeAxis, DataContainer
 import pandas as pds
 import numpy as np
 
@@ -57,7 +57,15 @@ def load_csv(filename: str) -> SpeasyVariable:
             max_v = np.array([float(v) for v in meta["PARAMETER_TABLE_MAX_VALUES[0]"].split(',')])
             y = (max_v + min_v) / 2.
             y_label = meta["PARAMETER_TABLE[0]"]
-        return SpeasyVariable(time=time, values=data, meta=meta, columns=columns[1:], extra_axes=[y], extra_axes_labels=[y_label])
+        time_axis = VariableTimeAxis(values=time)
+        if y is None:
+            axes = [time_axis]
+        else:
+            axes = [time_axis, VariableAxis(name='y', values=y, is_time_dependent=False)]
+        return SpeasyVariable(
+            axes=axes,
+            values=DataContainer(values=data, meta=meta),
+            columns=columns[1:])
 
 
 def _build_event(data, colnames: List[str]) -> Event:

tests/test_dataset.py
@@ -1,7 +1,7 @@
 import unittest
 
 from speasy.products.dataset import Dataset
-from speasy.products.variable import SpeasyVariable
+from speasy.products.variable import SpeasyVariable, VariableTimeAxis, DataContainer
 from speasy.core.datetime_range import DateTimeRange
 import numpy as np
 
@@ -9,8 +9,14 @@ import numpy as np
 def make_simple_var(start: float = 0., stop: float = 0., step: float = 1., coef: float = 1.):
     time = np.arange(start, stop, step)
     values = time * coef
-    return SpeasyVariable(time=SpeasyVariable.epoch_to_datetime64(time), values=values, meta=None, columns=["Values"],
-                          extra_axes=None)
+    return SpeasyVariable(axes=[VariableTimeAxis(values=SpeasyVariable.epoch_to_datetime64(time))],
+                          values=DataContainer(values=values, meta=None),
+                          columns=["Values"])
+
+
+def make_simple_dataset(name="test"):
+    return Dataset(name=name, variables={'v1': make_simple_var(), 'v2': make_simple_var(), 'v3': make_simple_var()},
+                   meta={})
 
 
 class SpeasyDataset(unittest.TestCase):
@@ -21,7 +27,7 @@ class SpeasyDataset(unittest.TestCase):
         pass
 
     def test_accepts_a_dict_of_variables(self):
-        ds = Dataset(name="test", variables={"v": SpeasyVariable()}, meta={})
+        ds = Dataset(name="test", variables={"v": make_simple_var()}, meta={})
         self.assertEqual(len(ds), 1)
         self.assertEqual("test", ds.name)
 
@@ -30,45 +36,39 @@ class SpeasyDataset(unittest.TestCase):
             ds = Dataset(name='Should raise', variables={'this is not a var': 10.}, meta={})
 
     def test_can_be_indexed_by_variable_name(self):
-        ds = Dataset(name="test", variables={"v": SpeasyVariable()}, meta={})
+        ds = Dataset(name="test", variables={"v": make_simple_var()}, meta={})
         self.assertIs(type(ds['v']), SpeasyVariable)
 
     def test_len_gives_variable_count(self):
         ds = Dataset(name="test", variables={}, meta={})
         self.assertEqual(len(ds), 0)
-        ds = Dataset(name="test", variables={'v1': SpeasyVariable(), 'v2': SpeasyVariable(), 'v3': SpeasyVariable()},
-                     meta={})
+        ds = make_simple_dataset()
         self.assertEqual(len(ds), 3)
 
     def test_has_str_repr(self):
-        ds = Dataset(name="test", variables={'v1': SpeasyVariable(), 'v2': SpeasyVariable(), 'v3': SpeasyVariable()},
-                     meta={})
+        ds =make_simple_dataset()
         repr = str(ds)
         self.assertIn('v1', repr)
         self.assertIn('v2', repr)
 
     def test_can_iterate_variables(self):
-        ds = Dataset(name="test", variables={'v1': SpeasyVariable(), 'v2': SpeasyVariable(), 'v3': SpeasyVariable()},
-                     meta={})
+        ds =make_simple_dataset()
         var_list = [v for v in ds]
         self.assertListEqual(['v1', 'v2', 'v3'], var_list)
 
     def test_returns_true_if_variable_name_is_in_dataset(self):
-        ds = Dataset(name="test", variables={'v1': SpeasyVariable(), 'v2': SpeasyVariable(), 'v3': SpeasyVariable()},
-                     meta={})
+        ds = make_simple_dataset()
         for v in ds:
             self.assertIn(v, ds)
 
     def test_returns_false_if_variable_name_is_not_in_dataset(self):
-        ds = Dataset(name="test", variables={'v1': SpeasyVariable(), 'v2': SpeasyVariable(), 'v3': SpeasyVariable()},
-                     meta={})
+        ds = make_simple_dataset()
         self.assertNotIn('Not in', ds)
 
     def test_has_no_time_range_when_empty_or_has_empty_vars(self):
         ds = Dataset(name="test", variables={}, meta={})
         self.assertIsNone(ds.time_range())
-        ds = Dataset(name="test", variables={'v1': SpeasyVariable(), 'v2': SpeasyVariable(), 'v3': SpeasyVariable()},
-                     meta={})
+        ds = make_simple_dataset()
         self.assertIsNone(ds.time_range())
 
     def test_has_a_time_range_encompassing_all_variables(self):