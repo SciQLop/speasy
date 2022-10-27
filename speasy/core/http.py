9a4332c7218be668f06c040b53dd10959848c850
Benjamin Renard <benjamin.renard@akka.eu>
2022-10-27 13:59:47 +0200

    Some fixes

--- speasy/core/http.py
+++ speasy/core/http.py
@@ -1,11 +1,11 @@
 from speasy import __version__
 import platform
 import requests
+import socket
 from requests.utils import quote as _quote
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-from urllib.request import Request, urlopen, HTTPError
-from socket import timeout
+from urllib.request import Request, urlopen, HTTPError, URLError
 from time import sleep
 import logging
 
@@ -13,13 +13,13 @@ log = logging.getLogger(__name__)
 
 USER_AGENT = f'Speasy/{__version__} {platform.uname()} (SciQLop project)'
 
-DEFAULT_TIMEOUT = 60 # seconds
+DEFAULT_TIMEOUT = 60  # seconds
 
-DEFAULT_DELAY = 5 # seconds
+DEFAULT_DELAY = 5  # seconds
 
 DEFAULT_RETRY_COUNT = 5
 
-STATUS_FORCE_LIST = [500, 502, 503, 504] # Note: Specific treatment for 429 error code (see below)
+STATUS_FORCE_LIST = [500, 502, 503, 504]  # Note: Specific treatment for 429 error code (see below)
 
 
 class TimeoutHTTPAdapter(HTTPAdapter):
@@ -40,6 +40,7 @@ class TimeoutHTTPAdapter(HTTPAdapter):
 def quote(*args, **kwargs):
     return _quote(*args, **kwargs)
 
+
 def get(url, headers: dict = None, params: dict = None):
     headers = {} if headers is None else headers
     headers['User-Agent'] = USER_AGENT
@@ -55,7 +56,7 @@ def get(url, headers: dict = None, params: dict = None):
     http.mount("https://", adapter)
     http.mount("http://", adapter)
     resp = http.get(url, headers=headers, params=params)
-    while resp.status_code == 429: # Honor "Retry-After"
+    while resp.status_code == 429:  # Honor "Retry-After"
         try:
             delay = float(resp.headers['Retry-After'])
         except ValueError:
@@ -64,6 +65,7 @@ def get(url, headers: dict = None, params: dict = None):
         sleep(delay)
         resp = http.get(url, headers=headers, params=params)
     return resp
+
 
 def urlopen_with_retry(url, timeout: int = DEFAULT_TIMEOUT, headers: dict = None):
     headers = {} if headers is None else headers
@@ -81,7 +83,7 @@ def urlopen_with_retry(url, timeout: int = DEFAULT_TIMEOUT, headers: dict = None
                 log.debug(f"Timeout exception during urlopen request, will sleep for {delay} seconds")
             elif e.code in STATUS_FORCE_LIST:
                 log.debug(f"HTTP Error Got {e.code} response, will sleep for {delay} seconds")
-            elif e.code == 429: # Honor "Retry-After"
+            elif e.code == 429:  # Honor "Retry-After"
                 try:
                     delay = float(resp.headers['Retry-After'])
                 except ValueError:
