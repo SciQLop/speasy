745f91765a0e934e5e7c5783e747f07c46b1774d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-12-05 09:24:22 +0100

    Bump version: 1.0.3 → 1.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,7 @@ author = u"Alexis Jeandet"
 # the built documents.
 #
 # The short X.Y version.
-version = '1.0.3'
+version = '1.0.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
