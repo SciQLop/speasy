c4e31174032d16c6d764611153625937d834ffaa
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-05-01 15:59:00 +0200

    Fixes (maybe unused) Parameter in Dataset? and Component in Parameter? code
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,9 +3,10 @@
 
 """Tests for `speasy.common` package."""
 import unittest
+
 from ddt import ddt, data, unpack
 
-import speasy.core
+import speasy as spz
 
 
 @ddt
@@ -23,4 +24,32 @@ class Listify(unittest.TestCase):
     )
     @unpack
     def test_listify(self, input, expected):
-        self.assertEqual(speasy.core.listify(input), expected)
+        self.assertEqual(spz.core.listify(input), expected)
+
+
+class Indexes(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_parameter_index_contains_component(self):
+        self.assertIn(spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all.imf_gsm.imf_gsm0,
+                      spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all.imf_gsm)
+
+        self.assertNotIn(spz.inventories.tree.amda.Parameters.ACE.MFI.ace_mag_real.imf_real_gse.imf_real_gse1,
+                         spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all.imf_gsm)
+
+        self.assertNotIn("random name that is not a component",
+                         spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all.imf_gsm)
+
+    def test_dataset_index_contains_parameter(self):
+        self.assertIn(spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all.imf_gsm,
+                      spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all)
+
+        self.assertNotIn(spz.inventories.tree.amda.Parameters.ACE.MFI.ace_mag_real.imf_real_gse,
+                         spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all)
+
+        self.assertNotIn("random name that is not a parameter",
+                         spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all)
