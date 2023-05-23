7d55ec99f18d1ce3ac95e33e62f4715d00cd7f6f
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-05-23 09:49:33 +0200

    Refac CDA inventory builder to use shared code from core.cdf inventory extractor
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy/webservices/cda/_inventory_builder/_cdf_masters_parser.py
+++ speasy/webservices/cda/_inventory_builder/_cdf_masters_parser.py
@@ -1,55 +1,28 @@
-import os.path
 import logging
+import os.path
 from typing import List
-import pyistp
+
+from speasy.core.cdf.inventory_extractor import extract_parameters
 from speasy.core.inventory.indexes import ParameterIndex, DatasetIndex, SpeasyIndex
 
 log = logging.getLogger(__name__)
 
 
-def extract_variable(variable):
-    return {
-        'shape': variable.shape[1:],
-        'attributes': {name: str(value) for name, value in variable.attrs.items()}
-    }
-
-
-def extract_variables(cdf):
-    return {
-        name: extract_variable(v) for name, v in cdf.items()
-    }
-
-
-def filter_meta(attributes):
-    keep_list = ['CATDESC', 'FIELDNAM', 'UNITS', 'UNIT_PTR', 'DISPLAY_TYPE', 'LABLAXIS', 'LABL_PTR_1', 'LABL_PTR_2',
-                 'LABL_PTR_3']
-    return {key: value for key, value in attributes.items() if key in keep_list}
+def _patch_parameter(parameter: ParameterIndex, dataset: DatasetIndex):
+    parameter.start_date = dataset.start_date
+    parameter.stop_date = dataset.stop_date
+    parameter.dataset = dataset.spz_uid()
+    return parameter
 
 
 def load_master_cdf(path, dataset: DatasetIndex):
-    skip_count = 0
-    try:
-        cdf = pyistp.load(path)
-        for name in cdf.data_variables():
-            try:
-                datavar = cdf.data_variable(name)
-                if datavar is not None:
-                    index = ParameterIndex(name=name, provider="cda", uid=f"{dataset.serviceprovider_ID}/{name}",
-                                           meta=filter_meta(datavar.attributes))
-                    index.start_date = dataset.start_date
-                    index.stop_date = dataset.stop_date
-                    index.dataset = dataset.spz_uid()
-                    dataset.__dict__[name] = index
-            except IndexError or RuntimeError:
-                print(f"Issue loading {name} from {path}")
-                skip_count += 1
-    except RuntimeError:
-        print(f"Issue loading {name} from {path}")
-        skip_count += 1
-    return skip_count
+    dataset.__dict__.update(
+        {p.spz_name(): p for p in
+         map(lambda p: _patch_parameter(p, dataset), extract_parameters(path, provider="cda",
+                                                                        uid_fmt=f"{dataset.serviceprovider_ID}/{{var_name}}"))})
 
 
-def _extract_datasets(root: SpeasyIndex):
+def _extract_datasets(root: SpeasyIndex) -> List[DatasetIndex]:
     def extract_datasets(node: SpeasyIndex, datasets: List):
         if isinstance(node, DatasetIndex):
             datasets.append(node)
@@ -63,13 +36,9 @@ def _extract_datasets(root: SpeasyIndex):
 
 
 def update_tree(root: SpeasyIndex, master_cdf_dir):
-    skip_count = 0
     datasets = _extract_datasets(root)
     for dataset in datasets:
         master_cdf_fname = dataset.mastercdf.split('/')[-1]
         full_path = os.path.join(master_cdf_dir, master_cdf_fname)
         if os.path.exists(full_path):
-            skip_count += load_master_cdf(full_path, dataset)
-        else:
-            skip_count += 1
-    print(f"{skip_count} datasets or variables skipped")
+            load_master_cdf(full_path, dataset)
