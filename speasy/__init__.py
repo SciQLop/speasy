df0adeb0b7b4d1b980dca385ca438d09626640e0
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-05-24 09:07:12 +0200

    fixes few misses on generic_archive integration
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy/__init__.py
+++ speasy/__init__.py
@@ -9,13 +9,14 @@
 __author__ = """Alexis Jeandet"""
 __email__ = 'alexis.jeandet@member.fsf.org'
 __version__ = '1.1.2'
-__all__ = ['amda', 'cda', 'ssc', 'csa', 'get_data', 'SpeasyVariable', 'Catalog', 'Event', 'Dataset', 'TimeTable']
+__all__ = ['amda', 'cda', 'ssc', 'csa', 'get_data', 'archive', 'SpeasyVariable', 'Catalog', 'Event', 'Dataset', 'TimeTable']
 __docformat__ = "numpy"
 
-from speasy.core.inventory.indexes import SpeasyIndex
-from .products import SpeasyVariable, Catalog, Event, Dataset, TimeTable, MaybeAnyProduct
 from typing import List
-from .core.requests_scheduling.request_dispatch import get_data, list_providers, amda, cda, csa, ssc
+
+from speasy.core.inventory.indexes import SpeasyIndex
+from .core.requests_scheduling.request_dispatch import get_data, list_providers, amda, cda, csa, ssc, archive
+from .products import SpeasyVariable, Catalog, Event, Dataset, TimeTable, MaybeAnyProduct
 
 
 # @TODO implement me, this function should be able to look inside all servers
