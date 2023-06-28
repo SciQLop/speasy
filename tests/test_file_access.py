eacfd7f5aff5e389fa9d32a0169f0de701acf1dd
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-06-28 11:46:10 +0200

    Yeah on CI network is almost as fast as disks...
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_file_access.py
+++ tests/test_file_access.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `speasy.common` package."""
+import os
 import re
 import unittest
-import os
-from ddt import ddt, data
 from datetime import datetime
-from speasy.core.cache import drop_item
+
+from ddt import ddt, data
+
 from speasy.core.any_files import any_loc_open, list_files
+from speasy.core.cache import drop_item
 
 _HERE_ = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,14 +31,16 @@ class FileAccess(unittest.TestCase):
     def test_cached_remote_txt_file(self):
         drop_item("https://hephaistos.lpp.polytechnique.fr/data/jeandet/Vbias.html")
         start = datetime.now()
-        f = any_loc_open("https://hephaistos.lpp.polytechnique.fr/data/jeandet/Vbias.html", mode='r',cache_remote_files=True)
+        f = any_loc_open("https://hephaistos.lpp.polytechnique.fr/data/jeandet/Vbias.html", mode='r',
+                         cache_remote_files=True)
         mid = datetime.now()
         self.assertIn('<!DOCTYPE html>', f.read())
-        f = any_loc_open("https://hephaistos.lpp.polytechnique.fr/data/jeandet/Vbias.html", mode='r',cache_remote_files=True)
+        f = any_loc_open("https://hephaistos.lpp.polytechnique.fr/data/jeandet/Vbias.html", mode='r',
+                         cache_remote_files=True)
         stop = datetime.now()
         self.assertIn('<!DOCTYPE html>', f.read())
         self.assertIsNotNone(f)
-        self.assertGreater((mid - start)/4, stop - mid)
+        self.assertGreater(mid - start, stop - mid)
 
     def test_simple_remote_bin_file(self):
         f = any_loc_open("https://hephaistos.lpp.polytechnique.fr/data/LFR/SW/LFR-FSW/3.0.0.0/fsw", mode='rb')
@@ -55,7 +59,7 @@ class FileAccess(unittest.TestCase):
         stop = datetime.now()
         self.assertEquals(b'\x7fELF', f.read(4))
         self.assertIsNotNone(f)
-        self.assertGreater((mid - start)/4, stop - mid)
+        self.assertGreater(mid - start, stop - mid)
 
     @data(
         f"{_HERE_}/resources/derived_param.txt",
