ce0d1a147a8cbc7dcc31c522a33e4a30b33a8c5d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-10-10 10:48:36 +0200

    Bump version: 1.1.2 → 1.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -123,7 +123,7 @@ author = u"Alexis Jeandet"
 # the built documents.
 #
 # The short X.Y version.
-version = '1.1.2'
+version = '1.1.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- speasy/__init__.py
+++ speasy/__init__.py
@@ -8,7 +8,7 @@
 
 __author__ = """Alexis Jeandet"""
 __email__ = 'alexis.jeandet@member.fsf.org'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 __all__ = ['amda', 'cda', 'ssc', 'csa', 'get_data', 'SpeasyVariable', 'Catalog', 'Event', 'Dataset', 'TimeTable']
 __docformat__ = "numpy"
 
