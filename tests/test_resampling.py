aa6cef472bfca046aa4a4d6b59fe5f17e7ac069b
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-04-23 16:00:27 +0200

    Allows custom interpolator with resampling module
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -11,18 +11,19 @@ import numpy as np
 
 from speasy.products import SpeasyVariable, VariableTimeAxis, DataContainer
 from speasy.signal.resampling import resample, generate_time_vector, interpolate
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 
 class TestGenerateTimeVector(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.dt = 500
-        self.start = datetime(2000, 1, 1, 1, 1)
-        self.stop = datetime(2000, 1, 1, 1, 2)
-        self.time_vector = generate_time_vector(self.start, self.stop, self.dt / 1000)
+    def setUpClass(cls):
+        cls.dt = 500
+        cls.start = datetime(2000, 1, 1, 1, 1)
+        cls.stop = datetime(2000, 1, 1, 1, 2)
+        cls.time_vector = generate_time_vector(cls.start, cls.stop, cls.dt / 1000)
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         pass
 
     def test_time_vector_length(self):
@@ -40,17 +41,17 @@ class TestGenerateTimeVector(unittest.TestCase):
 
 class TestDownSampling(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.start = datetime(2000, 1, 1, 1, 1)
-        self.stop = datetime(2000, 1, 1, 1, 2)
-        self.data = SpeasyVariable(
-            axes=[VariableTimeAxis(values=generate_time_vector(self.start, self.stop, 1))],
+    def setUpClass(cls):
+        cls.start = datetime(2000, 1, 1, 1, 1)
+        cls.stop = datetime(2000, 1, 1, 1, 2)
+        cls.data = SpeasyVariable(
+            axes=[VariableTimeAxis(values=generate_time_vector(cls.start, cls.stop, 1))],
             values=DataContainer(np.arange(60.).reshape(-1, 1))
         )
-        self.resampled = resample(self.data, 10)
+        cls.resampled = resample(cls.data, 10)
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         pass
 
     def test_resampled_length(self):
@@ -68,17 +69,17 @@ class TestDownSampling(unittest.TestCase):
 
 class TestUpSamplingSimpleSlope(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.start = datetime(2000, 1, 1, 1, 1)
-        self.stop = datetime(2000, 1, 1, 1, 2)
-        self.data = SpeasyVariable(
-            axes=[VariableTimeAxis(values=generate_time_vector(self.start, self.stop, 1))],
+    def setUpClass(cls):
+        cls.start = datetime(2000, 1, 1, 1, 1)
+        cls.stop = datetime(2000, 1, 1, 1, 2)
+        cls.data = SpeasyVariable(
+            axes=[VariableTimeAxis(values=generate_time_vector(cls.start, cls.stop, 1))],
             values=DataContainer(np.arange(60.).reshape(-1, 1))
         )
-        self.resampled = resample(self.data, .5)
+        cls.resampled = resample(cls.data, .5)
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         pass
 
     def test_resampled_length(self):
@@ -96,17 +97,17 @@ class TestUpSamplingSimpleSlope(unittest.TestCase):
 
 class TestUpSamplingSine(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.start = datetime(2000, 1, 1, 1, 1)
-        self.stop = datetime(2000, 1, 1, 1, 2)
-        self.data = SpeasyVariable(
-            axes=[VariableTimeAxis(values=generate_time_vector(self.start, self.stop, np.timedelta64(1, 'ms')))],
+    def setUpClass(cls):
+        cls.start = datetime(2000, 1, 1, 1, 1)
+        cls.stop = datetime(2000, 1, 1, 1, 2)
+        cls.data = SpeasyVariable(
+            axes=[VariableTimeAxis(values=generate_time_vector(cls.start, cls.stop, np.timedelta64(1, 'ms')))],
             values=DataContainer(np.sin(np.arange(0., 2 * np.pi, 2 * np.pi / 60000)).reshape(-1, 1))
         )
-        self.resampled = resample(self.data, np.timedelta64(500, 'us'))
+        cls.resampled = resample(cls.data, np.timedelta64(500, 'us'))
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         pass
 
     def test_resampled_length(self):
@@ -121,26 +122,26 @@ class TestUpSamplingSine(unittest.TestCase):
     def test_resampled_values(self):
         self.assertTrue(np.allclose(self.resampled.values,
                                     np.sin(np.arange(0., 2 * np.pi, 2 * np.pi / (60000 * 2)))[:-1].reshape(-1, 1),
-                                    rtol=1e-4))
+                                    rtol=3e-5))
 
 
 class TestInterpolateVariableOverAnotherVariable(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.start = datetime(2000, 1, 1, 1, 1)
-        self.stop = datetime(2000, 1, 1, 1, 2)
-        self.data = SpeasyVariable(
-            axes=[VariableTimeAxis(values=generate_time_vector(self.start, self.stop, 1))],
+    def setUpClass(cls):
+        cls.start = datetime(2000, 1, 1, 1, 1)
+        cls.stop = datetime(2000, 1, 1, 1, 2)
+        cls.data = SpeasyVariable(
+            axes=[VariableTimeAxis(values=generate_time_vector(cls.start, cls.stop, 1))],
             values=DataContainer(np.arange(60.).reshape(-1, 1))
         )
-        self.ref_var = SpeasyVariable(
-            axes=[VariableTimeAxis(values=generate_time_vector(self.start, self.stop, 2))],
+        cls.ref_var = SpeasyVariable(
+            axes=[VariableTimeAxis(values=generate_time_vector(cls.start, cls.stop, 2))],
             values=DataContainer(np.arange(30.).reshape(-1, 1))
         )
-        self.resampled = interpolate(self.data, ref_variable=self.ref_var)
+        cls.resampled = interpolate(cls.data, cls.ref_var)
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         pass
 
     def test_resampled_length(self):
@@ -152,3 +153,33 @@ class TestInterpolateVariableOverAnotherVariable(unittest.TestCase):
     def test_resampled_values(self):
         self.assertTrue(np.all(self.resampled.values == np.arange(0., 60., 2.).reshape(-1, 1)))
 
+
+class TestScipyInterpolator(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.start = datetime(2000, 1, 1, 1, 1)
+        cls.stop = datetime(2000, 1, 1, 1, 2)
+        cls.data = SpeasyVariable(
+            axes=[VariableTimeAxis(values=generate_time_vector(cls.start, cls.stop, np.timedelta64(1, 'ms')))],
+            values=DataContainer(np.sin(np.arange(0., 2 * np.pi, 2 * np.pi / 60000)).reshape(-1, 1))
+        )
+        cls.resampled = resample(cls.data, np.timedelta64(500, 'us'),
+                                 interpolate_callback=InterpolatedUnivariateSpline, k=4)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_resampled_length(self):
+        self.assertEqual(len(self.resampled.time), len(self.data.time) * 2 - 1)
+
+    def test_resampled_time_step(self):
+        self.assertTrue(np.all((self.resampled.time[1:] - self.resampled.time[0:-1]) == np.timedelta64(500, 'us')))
+
+    def test_resampled_values_length(self):
+        self.assertEqual(len(self.resampled.values), len(self.data.time) * 2 - 1)
+
+    def test_resampled_values(self):
+        self.assertTrue(np.allclose(self.resampled.values,
+                                    np.sin(np.arange(0., 2 * np.pi, 2 * np.pi / (60000 * 2)))[:-1].reshape(-1, 1),
+                                    rtol=4e-5))
