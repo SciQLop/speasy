fd2a427767eb97a85469a2110567315d0642b467
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-08-13 10:54:43 +0200

    Fix typo in datetime64_to_epoch and add some tests
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,6 +3,7 @@
 
 """Tests for `speasy.common` package."""
 import unittest
+import numpy as np
 
 from ddt import ddt, data, unpack
 
@@ -53,6 +54,35 @@ class Indexes(unittest.TestCase):
 
         self.assertNotIn("random name that is not a parameter",
                          spz.inventories.tree.amda.Parameters.ACE.MFI.ace_imf_all)
+
+
+@ddt
+class TestTimeConversions(unittest.TestCase):
+
+    @data(
+        (np.array(['1970-01-01T00:00:00.000000000', '1970-01-01T00:00:01.000000000'], dtype='datetime64[ns]'),
+         np.array([0., 1.])),
+        (np.array(['1970-01-01T00:00:00.000000000', '1970-01-01T00:00:01.000000001'], dtype='datetime64[ns]'),
+         np.array([0., 1.000000001])),
+        (np.array(['2040-01-01T00:00:00.000000000', '2040-01-01T00:00:01.000000001'], dtype='datetime64[ns]'),
+         np.array([2208985200.0, 2208985201.000000001])),
+    )
+    @unpack
+    def test_dt64_to_epoch(self, input, expected):
+        self.assertTrue(np.allclose(spz.core.datetime64_to_epoch(input), expected, atol=1e-10))
+
+    @data(
+        (np.array([0., 1.]),
+         np.array(['1970-01-01T00:00:00.000000000', '1970-01-01T00:00:01.000000000'], dtype='datetime64[ns]')),
+        (np.array([0., 1.000000001]),
+         np.array(['1970-01-01T00:00:00.000000000', '1970-01-01T00:00:01.000000001'], dtype='datetime64[ns]')),
+        (np.array([2208985200.0, 2208985201.000000001]),
+         np.array(['2040-01-01T00:00:00.000000000', '2040-01-01T00:00:01.000000001'], dtype='datetime64[ns]')),
+    )
+    @unpack
+    def test_epoch_to_dt64(self, input, expected):
+        self.assertTrue(
+            np.allclose(spz.core.epoch_to_datetime64(input).astype('int64'), expected.astype('int64'), atol=1e-10))
 
 
 if __name__ == '__main__':
