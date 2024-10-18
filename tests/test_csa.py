2a667af2eca9eeec22d19a3a9a90d7fb9d49a84e
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-10-18 10:09:55 +0200

    Update number of available products from CSA
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_csa.py
+++ tests/test_csa.py
@@ -9,7 +9,7 @@ class CSAInventory(unittest.TestCase):
         os.environ[spz.config.proxy.enabled.env_var_name] = "False"
         spz.csa.update_inventory()
         os.environ.pop(spz.config.proxy.enabled.env_var_name)
-        self.assertGreaterEqual(len(spz.inventories.flat_inventories.csa.parameters), 1993)
+        self.assertGreaterEqual(len(spz.inventories.flat_inventories.csa.parameters), 1985)
         self.assertIn('flux__C1_CP_CIS_HIA_HS_1D_PEF',
                       spz.inventories.tree.csa.Cluster.Cluster_1.CIS_HIA1.C1_CP_CIS_HIA_HS_1D_PEF.__dict__)
 
