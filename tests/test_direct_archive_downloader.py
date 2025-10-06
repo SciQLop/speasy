55e4b2bad3a7dcc287632c2fddfbad0956a70472
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-10-06 17:33:56 +0200

    Cache file timerange maping for direct archive modules
    For each folder Speasy will extract files time range only once and cahe the result. Each request targeting the same folder will just 
    recompute the intersection between the requested range and files ranges.
    
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_direct_archive_downloader.py
+++ tests/test_direct_archive_downloader.py
@@ -6,7 +6,7 @@ import speasy as spz
 from speasy.core import make_utc_datetime
 from speasy.core.cdf.inventory_extractor import extract_parameters
 from speasy.core.direct_archive_downloader import get_product
-from speasy.core.direct_archive_downloader.direct_archive_downloader import spilt_range, _read_cdf
+from speasy.core.direct_archive_downloader.direct_archive_downloader import spilt_range, _read_cdf, map_ranges
 
 
 def _custom_cdf_loader(url, variable, *args, **kwargs):
@@ -57,15 +57,15 @@ class DirectArchiveDownloader(unittest.TestCase):
 
     @data(
         (
-            "https://cdaweb.gsfc.nasa.gov/pub/data/arase/pwe/hfa/l3/1min/{Y}/erg_pwe_hfa_l3_1min_{Y}{M:02d}{D:02d}_v04_09.cdf",
-            "regular", "ne_mgf", "2018-02-01", "2018-02-02"),
+                "https://cdaweb.gsfc.nasa.gov/pub/data/arase/pwe/hfa/l3/1min/{Y}/erg_pwe_hfa_l3_1min_{Y}{M:02d}{D:02d}_v04_09.cdf",
+                "regular", "ne_mgf", "2018-02-01", "2018-02-02"),
         (
-            "http://themis.ssl.berkeley.edu/data/themis/thb/l2/scm/{Y}/thb_l2_scm_{Y}{M:02d}{D:02d}_v01.cdf",
-            "regular", "thb_scf_gse", "2008-07-23T10", "2008-07-23T15"),
+                "http://themis.ssl.berkeley.edu/data/themis/thb/l2/scm/{Y}/thb_l2_scm_{Y}{M:02d}{D:02d}_v01.cdf",
+                "regular", "thb_scf_gse", "2008-07-23T10", "2008-07-23T15"),
         (
-            "https://cdaweb.gsfc.nasa.gov/pub/data/solar-orbiter/rpw/low_latency/ll02/sbm1/{Y}/solo_ll02_rpw-sbm1_{Y}{M:02d}{D:02d}t\d+-\d+t\d+_v\d+u.cdf",
-            "random", "DT1_SBM1", "2021-07-23T10", "2021-07-25T23",
-            {'fname_regex': 'solo_ll02_rpw-sbm1_(?P<start>\d+t\d+)-(?P<stop>\d+t\d+)_v(?P<version>\d+)u\.cdf'}),
+                r"https://cdaweb.gsfc.nasa.gov/pub/data/solar-orbiter/rpw/low_latency/ll02/sbm1/{Y}/solo_ll02_rpw-sbm1_{Y}{M:02d}{D:02d}t\d+-\d+t\d+_v\d+u.cdf",
+                "random", "DT1_SBM1", "2021-07-23T10", "2021-07-25T23",
+                {'fname_regex': r'solo_ll02_rpw-sbm1_(?P<start>\d+t\d+)-(?P<stop>\d+t\d+)_v(?P<version>\d+)u\.cdf'}),
     )
     @unpack
     def test_download_existing_data(self, url_pattern, split_rule, variable, start_time, stop_time, kwargs=None):
@@ -151,10 +151,41 @@ class DirectArchiveDownloader(unittest.TestCase):
         self.assertIsNone(v)
 
     def test_axes_merging_across_files(self):
-        v = spz.get_data("archive/cda/MMS/MMS1/FPI/FAST/MOMS/mms1_fpi_fast_l2_des_moms/mms1_des_energyspectr_omni_fast", "2018-01-05",
+        v = spz.get_data("archive/cda/MMS/MMS1/FPI/FAST/MOMS/mms1_fpi_fast_l2_des_moms/mms1_des_energyspectr_omni_fast",
+                         "2018-01-05",
                          "2018-01-07")
         self.assertIsNotNone(v)
         self.assertEqual(len(v), len(v.axes[1]))
+
+    def test_map_ranges_simple_case(self):
+        ranges = map_ranges(
+            url="https://sciqlop.lpp.polytechnique.fr/cdaweb-data/pub/data/ace/mag/level_2_cdaweb/mfi_h0/2010/.*.cdf",
+            fname_regex=r"ac_h0_mfi_(?P<start>\d+)_v(?P<version>\d+).cdf",
+            date_format="%Y%m%d", force_refresh=True)
+        self.assertEqual(len(ranges), 365)
+        self.assertEqual(ranges[0][1], (make_utc_datetime("2010-01-01"), make_utc_datetime("2010-01-02")))
+        self.assertEqual(ranges[-1][1], (make_utc_datetime("2010-12-31"), None))
+
+    def test_map_ranges_burst_files(self):
+        ranges = map_ranges(
+            url="https://sciqlop.lpp.polytechnique.fr/cdaweb-data/pub/data/mms/mms1/scm/brst/l2/schb/2021/07/.*.cdf",
+            fname_regex=r"mms1_scm_brst_l2_schb_(?P<start>\d+)_v(?P<version>[\d\.]+).cdf",
+            date_format="%Y%m%d%H%M%S", force_refresh=True)
+        self.assertEqual(len(ranges), 841)
+        self.assertEqual(ranges[0][1],
+                         (make_utc_datetime("2021-07-01T00:03:43"), make_utc_datetime("2021-07-01T01:13:13")))
+        self.assertEqual(ranges[-1][1],
+                         (make_utc_datetime("2021-07-31T23:58:33"), None))
+
+    def test_map_ranges_with_start_and_stop_in_filename(self):
+        ranges = map_ranges(
+            url="https://sciqlop.lpp.polytechnique.fr/cdaweb-data/pub/data/solar-orbiter/swa/science/l2/eas1-tm3d-psd/2024/.*cdf",
+            fname_regex=r"solo_l2_swa-eas1-tm3d-psd_(?P<start>\d+t\d+)-(?P<stop>\d+t\d+)_v(?P<version>\d+)\.cdf",
+            date_format="%Y%m%dT%H%M%S",
+            force_refresh=True)
+        self.assertEqual(len(ranges), 256)
+        self.assertEqual(ranges[0][1],
+                         (make_utc_datetime("2024-01-04T05:31:42"), make_utc_datetime("2024-01-04T05:36:40")))
 
 
 if __name__ == '__main__':
