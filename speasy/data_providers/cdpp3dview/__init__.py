fe18f0a5b3c6a09af8df5ca039a39e086233e6a7
Richard Hitier <hitier.richard@gmail.com>
2026-01-08 16:41:16 +0100

    Add converters 3dview json to ParamIndex converters
    - body to ParamIndex
    - frame to ParamIndex
    
    used in cdpp3dview.build_inventory()

--- speasy/data_providers/cdpp3dview/__init__.py
+++ speasy/data_providers/cdpp3dview/__init__.py
@@ -6,19 +6,74 @@ __author__ = """Richard Hitier"""
 __email__ = "hitier.richard@gmail.com"
 __version__ = "0.1.0"
 
-import logging
-from typing import Dict, Optional
 import json
+import logging
+from typing import Dict
 from urllib.request import urlopen
 
 from speasy.core.dataprovider import DataProvider
-from speasy.core.inventory.indexes import SpeasyIndex
+from speasy.core.inventory.indexes import ParameterIndex, SpeasyIndex
 
 log = logging.getLogger(__name__)
 
 
+def body_to_paramindex(body: Dict) -> ParameterIndex:
+    """ Build a ParameterIndex from a body dictionary.
+
+    Parameters
+    ----------
+    body : Dict
+        A body from the cddp 3dview webservice.
+
+    Returns
+    -------
+    ParameterIndex
+        the corresponding ParameterIndex.
+    """
+    name = body.pop("name")
+    body["Id"] = body.pop("id")
+    coverage = body.pop("coverage")
+    body["start_date"] = coverage[0]
+    body["stop_date"] = coverage[1]
+    node = ParameterIndex(name=name, provider="ssc", uid=body["Id"], meta=body)
+    return node
+
+
+def frame_to_paramindex(frame: Dict) -> SpeasyIndex:
+    """ Build a ParameterIndex from a frame dictionary.
+
+    Parameters
+    ----------
+    frame : Dict
+        A frame from the cddp 3dview webservice.
+
+    Returns
+    -------
+    SpeasyIndex
+        the corresponding SpeasyIndex.
+    """
+    name = frame.pop("name")
+    frame["Id"] = frame.pop("id")
+    frame["Desc"] = frame.pop("desc")
+    frame["Center"] = frame.pop("center")
+    node = ParameterIndex(name=name, provider="ssc", uid=frame["Id"],
+                          meta=frame)
+    return node
+
 
 class Cdpp3dViewWebservice(DataProvider):
+    """ Cdpp3dViewWebservice Class
+
+    Parameters
+    ----------
+    DataProvider : speasy.core.dataprovider.DataProvider
+        core DataProvider class
+
+    Returns
+    -------
+    Cdpp3dViewWebservice
+        Cdpp3dViewWebservice instance
+    """
     BASE_URL = "https://3dview.irap.omp.eu/webresources"
 
     def __init__(self):
@@ -27,10 +82,20 @@ class Cdpp3dViewWebservice(DataProvider):
         )
 
     def build_inventory(self, root: SpeasyIndex):
-        # inv = list(map(make_index, self.get_observatories()))
-        # root.Trajectories = SpeasyIndex(name='Trajectories', provider='ssc',
-        #                                 uid='Trajectories',
-        #                                 meta={item.Id: item for item in inv})
+        bodies_index = list(map(body_to_paramindex, self._get_bodies()))
+        root.Bodies = SpeasyIndex(
+            name="Bodies",
+            provider="ssc",
+            uid="Bodies",
+            meta={item.Id: item for item in bodies_index},
+        )
+        frames_index = list(map(frame_to_paramindex, self._get_frames()))
+        root.Frames = SpeasyIndex(
+            name="Frames",
+            provider="ssc",
+            uid="Frames",
+            meta={item.Id: item for item in frames_index},
+        )
         return root
 
     def version(self, product):
