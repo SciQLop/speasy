b2ae790df3d49469c0572d7d232ef3b915c68fee
Richard Hitier <hitier.richard@gmail.com>
2026-01-14 20:11:06 +0100

    Add TODO items, remove imports, remove obsolete method

--- speasy/data_providers/cdpp3dview/__init__.py
+++ speasy/data_providers/cdpp3dview/__init__.py
@@ -6,11 +6,8 @@ __author__ = """Richard Hitier"""
 __email__ = "hitier.richard@gmail.com"
 __version__ = "0.1.0"
 
-import json
 import logging
-from typing import Dict, Optional, Tuple
-
-import numpy as np
+from typing import Optional
 
 from speasy import SpeasyVariable
 from speasy.core.data_containers import DataContainer, VariableTimeAxis
@@ -20,21 +17,6 @@ from speasy.core.inventory.indexes import ParameterIndex, SpeasyIndex
 from ...core.http import urlopen
 
 log = logging.getLogger(__name__)
-
-
-def parse_trajectory_json(json_data: str) -> Tuple[np.ndarray, np.ndarray]:
-    data = json.loads(json_data)
-
-    entries = data['values']
-
-    time_axis = np.array([np.datetime64(entry['time'][:-1], 'ns')
-                          for entry in entries])
-
-    positions = np.array([entry['position'] for entry in entries])
-    speeds = np.array([entry['speed'] for entry in entries])
-    values = np.concatenate([positions, speeds], axis=1)
-
-    return time_axis, values
 
 
 class Cdpp3dViewWebservice(DataProvider):
@@ -58,12 +40,20 @@ class Cdpp3dViewWebservice(DataProvider):
             self, provider_name="cdpp3dview", provider_alt_names=["cdpp3d"]
         )
 
+    # TODO: add decorators
+    # @UnversionedProviderCache(prefix="cdpp3dview", fragment_hours=24)
+    # @Proxyfiable(GetProduct, get_parameter_args_ws)
+    # @AllowedKwargs(PROXY_ALLOWED_KWARGS + CACHE_ALLOWED_KWARGS + GET_DATA_ALLOWED_KWARGS + ['sampling'])
+    # @EnsureUTCDateTime()
+    # @ParameterRangeCheck()
+    # TODO: change signature
     def get_data(self, body: str, frame: str, start: str, stop: str,
                  sampling: int = 3600,
                  format: str = "json") -> Optional[SpeasyVariable]:
         time_axis, values = self._get_trajectory(body, frame, start, stop,
                                                  sampling, format)
         try:
+            # TODO: Maybe should come from _get_trajectory ?
             return SpeasyVariable(
                 axes=[VariableTimeAxis(values=time_axis)],
                 values=DataContainer(values,
@@ -75,6 +65,7 @@ class Cdpp3dViewWebservice(DataProvider):
             log.error(f"Error parsing trajectory: {e}")
         return None
 
+    # TODO: move to _inventory_builder.build_inventory
     def build_inventory(self, root: SpeasyIndex):
         from speasy.core import fix_name
         from speasy.core.inventory.indexes import make_inventory_node
@@ -128,7 +119,7 @@ class Cdpp3dViewWebservice(DataProvider):
                     stop_date=body['coverage'][1]
                 )
 
-                # Filtrer les frames compatibles avec ce body
+                # TODO: Filter frames ?
                 compatible_frames = [
                     f for f in frames
                     if f.get('center') in ['Sun', body_name, 'Earth', '']
@@ -143,7 +134,7 @@ class Cdpp3dViewWebservice(DataProvider):
                         uid=f"{body_name}/{frame_name}",
                         meta=frame
                     )
-                    # Ajouter directement comme attribut
+                    # TODO: Add as attr ?
                     setattr(body_node, fix_name(frame_name), param)
 
         return root
@@ -167,6 +158,9 @@ class Cdpp3dViewWebservice(DataProvider):
 
         return data["frames"]
 
+    # TODO: write accordingly to 3dview REST api
+    #       get cdf format
+    #       build  and return SpeasyVariable ?
     def _get_trajectory(self, body, frame, start, stop, sampling=3600,
                         format="json"):
         URL = (
