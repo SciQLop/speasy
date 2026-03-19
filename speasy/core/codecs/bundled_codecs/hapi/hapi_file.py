19f04da71d92f3c50df7a11d25f17e6052c93bc6
Richard Hitier <hitier.richard@gmail.com>
2026-03-19 09:53:22 +0100

    Refactor: common hapi/hapi_file.py

--- speasy/core/codecs/bundled_codecs/hapi/hapi_file.py
+++ speasy/core/codecs/bundled_codecs/hapi/hapi_file.py
@@ -4,7 +4,7 @@ import logging
 import numpy as np
 log = logging.getLogger(__name__)
 
-class HapiBinaryParameter:
+class HapiParameter:
     def __init__(self, values: np.ndarray, meta: Dict[str, Any]):
         self._meta = meta
         self._values = values
@@ -22,16 +22,16 @@ class HapiBinaryParameter:
         return self._values
 
 
-class HapiBinaryFile:
+class HapiFile:
     def __init__(self):
-        self._parameters: List[HapiBinaryParameter] = []
+        self._parameters: List[HapiParameter] = []
 
-    def create_parameter(self, values: np.ndarray, meta: Dict[str, Any])->HapiBinaryParameter:
-        parameter = HapiBinaryParameter(values, meta)
+    def create_parameter(self, values: np.ndarray, meta: Dict[str, Any])->HapiParameter:
+        parameter = HapiParameter(values, meta)
         self._parameters.append(parameter)
         return parameter
 
-    def add_parameter(self, parameter: HapiBinaryParameter):
+    def add_parameter(self, parameter: HapiParameter):
         self._parameters.append(parameter)
 
     @property
@@ -42,7 +42,7 @@ class HapiBinaryFile:
     def time_axis_meta(self):
         return self._parameters[0].meta
 
-    def get_parameter(self, name: str) -> Optional[HapiBinaryParameter]:
+    def get_parameter(self, name: str) -> Optional[HapiParameter]:
         for par in self._parameters:
             if par.name == name:
                 return par
