1f8388f08fdee34b70ac6eb52bebb445c12e7092
Richard Hitier <hitier.richard@gmail.com>
2026-03-23 18:17:24 +0100

    Hapi Provider Skeleton

---
+++ speasy/core/hapi/client.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from typing import Optional
+
+from speasy.core import http
+
+from .exceptions import HapiError, HapiRequestError, HapiServerError, HapiNoData
+
+
+class HapiEndpoint(Enum):
+    CAPABILITIES = "capabilities"
+    CATALOG      = "catalog"
+    INFO         = "info"
+    DATA         = "data"
+    ABOUT        = "about"
+
+
+class HapiClient:
+    def __init__(self, server_url: str):
+        self.server_url = server_url.rstrip('/')
+
+    def _build_url(self, endpoint: HapiEndpoint) -> str:
+        url = f"{self.server_url}/hapi/{endpoint.value}"
+        return url
+
+    def _check_status(self, response_json: dict) -> None:
+        """Lit response_json['status'], lève la bonne HapiError si besoin."""
+        ...
+
+    def get_capabilities(self) -> dict: ...
+
+    def get_catalog(self) -> dict: ...
+
+    def get_info(self, dataset: str) -> dict: ...
+
+    def get_data(self, dataset: str, start: str, stop: str,
+                 parameters: Optional[str] = None) -> bytes: ...
+
+    def get_about(self) -> dict:
+        url = self._build_url(HapiEndpoint.ABOUT)
+        with http.urlopen(url, headers={"Accept": "application/json"}) as response:
+            data = response.json()
+        return data
+
