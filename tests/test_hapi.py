4a7f91dffdab95f82b917cc19454820887813cf1
Richard Hitier <hitier.richard@gmail.com>
2026-03-24 09:44:07 +0100

    Add capabilities() endpoint

--- tests/test_hapi.py
+++ tests/test_hapi.py
@@ -1,21 +1,21 @@
 
+import unittest
+
 from speasy.core.hapi.client import HapiClient, HapiEndpoint
 from speasy.core.hapi.provider import HapiProvider
 from speasy.core.hapi.exceptions import HapiRequestError, HapiServerError, HapiNoData
 
 
-AMDA_SERVER_URL = "https://amda.irap.omp.eu/service"
-HAPITEST33_SERVER_URL = "https://hapi-server.org/servers/TestData3.3/"
+AMDA_SERVER_ROOT = "https://amda.irap.omp.eu/service"
+HAPITEST33_SERVER_ROOT = "https://hapi-server.org/servers/TestData3.3"
+CDAWEB_SERVER_ROOT = "https://cdaweb.gsfc.nasa.gov"
 
+class TestHapiClient(unittest.TestCase):
 
-class TestHapiClient:
-
-    def test_build_url_capabilities(self): 
-        hapi_client = HapiClient(HAPITEST33_SERVER_URL)
-        result = hapi_client.get_about()
-        assert result["HAPI"] == "3.3"
-        assert result["status"]["code"] == 1200
-        assert isinstance(result["note"], list)
+    def test_build_url(self):
+        hapi_client = HapiClient(AMDA_SERVER_ROOT)
+        amda_about_url = hapi_client._build_url(endpoint=HapiEndpoint.ABOUT)
+        self.assertEqual(f"{AMDA_SERVER_ROOT}/hapi/about", amda_about_url)
 
     def test_build_url_info_with_dataset(self): ...
     def test_build_url_data_with_params(self): ...
@@ -31,17 +31,24 @@ class TestHapiClient:
     def test_check_status_raises_hapi_no_data_on_1201(self): ...
     def test_check_status_does_not_raise_on_1200(self): ...
 
-class TestHapiProvider:
+class TestHapiProvider(unittest.TestCase):
 
-    def test_capabilities_returns_dict(self): ...
+    def test_capabilities_returns_dict(self):
+        hapi_provider = HapiProvider(CDAWEB_SERVER_ROOT)
+        result = hapi_provider.capabilities()
+        from pprint import pprint
+        self.assertEqual( result["HAPI"], "2.0")
+        self.assertCountEqual( result["outputFormats"], ['json', 'csv', 'binary'])
+
+    def test_about_returns_dict(self):
+        hapi_provider = HapiProvider(HAPITEST33_SERVER_ROOT)
+        result = hapi_provider.about()
+        self.assertEqual(result["HAPI"], "3.3")
+        self.assertEqual( result["status"]["code"], 1200)
+        self.assertIsInstance(result["note"], list)
+
     def test_catalog_returns_dict(self): ...
     def test_info_returns_dict(self): ...
-    def test_about_returns_dict(self):
-        hapi_provider = HapiProvider(HAPITEST33_SERVER_URL)
-        result = hapi_provider.about()
-        assert result["HAPI"] == "3.3"
-        assert result["status"]["code"] == 1200
-        assert isinstance(result["note"], list)
 
     def test_data_returns_speasy_variable(self): ...
 
