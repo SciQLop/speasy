158a2f9f0e20e4ac1abf1caa1ab50895fae8e615
Richard Hitier <hitier.richard@gmail.com>
2026-03-25 10:31:23 +0100

    Catch and check data() endpoint errors

--- speasy/core/hapi/parser.py
+++ speasy/core/hapi/parser.py
@@ -5,9 +5,7 @@ from speasy.products.variable import SpeasyVariable
 class HapiParser:
 
     @staticmethod
-    def csv_to_variable(raw: bytes, info: dict) -> Optional[SpeasyVariable]:
-        """Convertit le CSV retourné par /data en SpeasyVariable.
-        
-        info est le dict retourné par /info (contient les métadonnées des paramètres).
+    def csv_to_variable(raw: bytes) -> Optional[SpeasyVariable]:
+        """Converts the CSV returned by /data into a SpeasyVariable.
         """
         ...
