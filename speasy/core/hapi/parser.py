c76373edfa3ebab81ac4ebef28302471dddf5a37
Richard Hitier <hitier.richard@gmail.com>
2026-03-26 13:20:51 +0100

    Refactor: extract static utils

--- speasy/core/hapi/parser.py
+++ speasy/core/hapi/parser.py
@@ -1,11 +1,18 @@
-from typing import Optional
+import io
+from typing import List
+from speasy.core.codecs.codec_interface import CodecInterface
+from speasy.core.codecs.codecs_registry import get_codec
+from speasy.core.hapi.exceptions import HapiError
 from speasy.products.variable import SpeasyVariable
 
 
-class HapiParser:
-
-    @staticmethod
-    def csv_to_variable(raw: bytes) -> Optional[SpeasyVariable]:
-        """Converts the CSV returned by /data into a SpeasyVariable.
-        """
-        ...
+def _parse_hapi_csv( file: io.IOBase, parameters: List[str]) -> List[SpeasyVariable]:
+    """Converts the CSV returned by /data into a SpeasyVariable.
+    """
+    if not parameters:
+        raise HapiError(
+            f"Wrong 'parameters' argument to hapi.load_variables: {parameters}"
+        )
+    hapi_csv_codec: CodecInterface = get_codec('hapi/csv')
+    variables = hapi_csv_codec.load_variables(file=file, variables=parameters, disable_cache=True)
+    return variables
